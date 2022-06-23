3c41e1711275c3627c8ffc3b4befcfabeafef9dd
Chris Hui <christopher.slhui@gmail.com>
2022-06-23 15:30:45 +0100

    Update index.js

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -492,9 +492,6 @@ function bookAppointment(handlerInput) {
 
 /* LAMBDA SETUP */
 
-// The SkillBuilder acts as the entry point for your skill, routing all request and response
-// payloads to the handlers above. Make sure any new handlers or interceptors you've
-// defined are included below. The order matters - they're processed top to bottom.
 exports.handler = Alexa.SkillBuilders.custom()
   .addRequestHandlers(
     InvalidConfigHandler,
